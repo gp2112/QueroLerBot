4bd870ab8bd4299cbd69c4da1563ded719a86cd8
gp2112 <guipaixao2000@gmail.com>
2021-03-16 17:31:11 -0300

    add tweet link without reference option

--- app.py
+++ app.py
@@ -105,29 +105,31 @@ if __name__ == '__main__':
 			for i, tweet in enumerate(mentions['data']):
 				user_name = mentions['includes']['users'][i]['username']
 				if 'referenced_tweets' not in tweet:
-					continue
-				t = twitter.get_tweet([tweet['referenced_tweets'][0]['id']])
-				print(f"{tweet['id']} {t[0]['text']} ")
-
-				url = re.search("(?P<url>https?://[^\s]+)", t[0]['text']).group("url")
-				if url is None:
-					#twitter.send_tweet(f'@{user_name} '+errors['url_not_found'], reply_to=tweet['id'])
-					pass
+					url = re.search("(?P<url>https?://[^\s]+)", tweet['text']).group("url")
+					if url is None:
+						continue
 				else:
-					url = real_url(url) # change Twitter's URL for the original one
-					url = urlparse(url)
-					url = f'{url.scheme}://{url.netloc}{url.path}' # erase GET params ;)
-					article = database.get_article(main_url=url)
-					if article is not None:
-						r = twitter.send_tweet(f'@{user_name} '+success(article[0]), reply_to=tweet['id'])
-						#print(r)
+					t = twitter.get_tweet([tweet['referenced_tweets'][0]['id']])
+					print(f"{tweet['id']} {t[0]['text']} ")
+
+					url = re.search("(?P<url>https?://[^\s]+)", t[0]['text']).group("url") # Gets the first URL
+					if url is None:
+						#twitter.send_tweet(f'@{user_name} '+errors['url_not_found'], reply_to=tweet['id'])
+						continue
+				url = real_url(url) # change Twitter's URL for the original one
+				url = urlparse(url)
+				url = f'{url.scheme}://{url.netloc}{url.path}' # erase GET params ;)
+				article = database.get_article(main_url=url)
+				if article is not None:
+					r = twitter.send_tweet(f'@{user_name} '+success(article[0]), reply_to=tweet['id'])
+					#print(r)
+				else:
+					article = antipay.break_paywall(url)
+					if article is None:
+						twitter.send_tweet(f'@{user_name} '+errors['text_not_found'], reply_to=tweet['id'])
 					else:
-						article = antipay.break_paywall(url)
-						if article is None:
-							twitter.send_tweet(f'@{user_name} '+errors['text_not_found'], reply_to=tweet['id'])
-						else:
-							database.insert_article(**article)
-							r = twitter.send_tweet(f'@{user_name} '+success(article['telegraph']), reply_to=tweet['id'])
+						database.insert_article(**article)
+						r = twitter.send_tweet(f'@{user_name} '+success(article['telegraph']), reply_to=tweet['id'])
 							#print(r)
 			print('Agurdando Tweets...')
 
