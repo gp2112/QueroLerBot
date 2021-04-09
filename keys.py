69bf3685d487a64dec05f4e4dad9884484b8e271
Ubuntu <ubuntu@ip-172-26-14-29.ec2.internal>
2021-04-09 14:45:25 +0000

    erase keys

--- keys.py
+++ keys.py
@@ -1,6 +1,6 @@
-consumer_key = 'LlvuNwtGH8by5h3k0fLa7OPCV' 
-consumer_secret = 'iMq9BTm0FtHDzt4K4jWfItQHD1AwalbjtnlfN0E1jCxr8zoXxZ'
-access_token_key = '1371527601316057100-965A42fpcDdTSiOdCDspfYyl2pkjMj' 
-access_token_secret = 'NrPxZKbHRJEGSOFNCpB1JcrWpmaxhfn9typgR0QshaQ5m'
+consumer_key = '' 
+consumer_secret = ''
+access_token_key = '' 
+access_token_secret = ''
 #Bearer token (sem 'Bearer')
-token = 'AAAAAAAAAAAAAAAAAAAAAIO3NgEAAAAAFQcmZ27%2BZhRWdOwUA4Xz8WNl2PQ%3DiWxqQZFfe3R9om4jviwrRypt91pLNGGmzVktNbo4r3XtAKsSBL'
+token = ''
